b66782eb791d13d138746386271c14d1d3446442
jcarter <Running-97-miles>
2022-05-26 15:01:45 -0700

    Fixed minor user interface odd behaviors.

--- ui.py
+++ ui.py
@@ -44,9 +44,9 @@ def select_input_file():
     initial_directory = r.load_str(key=key).strip()
 
     filename = fd.askopenfilename(title='File', initialdir=initial_directory, filetypes=filetypes)
-    input_file.set(value=filename)
 
     if len(filename) > 0:
+        input_file.set(value=filename)
         initial_directory = os.path.dirname(filename)
         r.save_str(key=key, value=initial_directory)
 
@@ -62,9 +62,9 @@ def select_output_file():
     initial_directory = r.load_str(key=key).strip()
 
     filename = fd.asksaveasfilename(title='Destination File', initialdir=initial_directory, filetypes=filetypes)
-    output_file.set(value=filename)
 
     if len(filename) > 0:
+        output_file.set(value=filename)
         initial_directory = os.path.dirname(filename)
         r.save_str(key=key, value=initial_directory)
 
@@ -86,6 +86,18 @@ def process_file():
 
     PROCESS(input_file_name=inp_file_name, output_file_name=out_file_name, account=account_no)
     messagebox.showinfo('Process', 'Process completed.')
+    return
+
+
+def input_file_callback(*args):
+    global inp_file_name
+    inp_file_name = input_file.get()
+    return
+
+
+def output_file_callback(*args):
+    global out_file_name
+    out_file_name = output_file.get()
     return
 
 
@@ -112,7 +124,6 @@ def load_inputs():
     account_no = r.load_str(key='account_no')
     return
 
-
 load_inputs()
 
 lbl_title = tk.Label(root, text='Positive Pay - File Converter')
@@ -124,6 +135,7 @@ lbl_input_file.place(x=10, y=row_y(2))
 
 input_file = StringVar()
 input_file.set(value=inp_file_name)
+input_file.trace_add('write', input_file_callback)
 inp_entry = tk.Entry(root, textvariable=input_file)
 inp_entry.place(x=70, y=row_y(2), width=350)
 
@@ -136,6 +148,7 @@ lbl_output_file.place(x=10, y=row_y(3))
 
 output_file = StringVar()
 output_file.set(value=out_file_name)
+output_file.trace_add('write', output_file_callback)
 out_entry = tk.Entry(root, textvariable=output_file)
 out_entry.place(x=90, y=row_y(3), width=330)
 
@@ -158,7 +171,6 @@ save_btn.place(x=10, y=row_y(6), height=22)
 # Process Button
 process_btn = ttk.Button(root, text="Process", command=process_file)
 process_btn.place(x=450, y=row_y(6), height=22)
-
 
 #
 # Setup window resize event, so we can keep buttons near lower edge of
